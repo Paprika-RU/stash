6235350301845ab309f4dbe2f832e85237e2307f
Paprika-RU <137256422+Paprika-RU@users.noreply.github.com>
2023-12-21 15:40:27 +0900

    Update ZooManagement.java

--- Zoo/ZooManagement.java
+++ Zoo/ZooManagement.java
@@ -7,19 +7,30 @@ public class ZooManagement {
         lion.makeSound();
         lion.eat();
 
+        // Создаем экземпляр класса Panther
+        Panther panther = new Panther();
+        panther.sleep();
+        panther.makeSound();
+        panther.eat();
+
         // Создаем экземпляр класса Dog
-        Dog dog = new Dog("Buddy");
+        Dog dog = new Dog("Fluffy");
         dog.sleep(); 
         dog.makeSound();
         dog.eat();
 
+        // Создаем экземпляр класса Cat
+        Cat cat = new Cat("Fiona");
+        cat.sleep(); 
+        cat.makeSound();
+        cat.eat();
 
         Animal animal1 = new Lion();
         animal1.sleep();
         animal1.makeSound();
         animal1.eat();
 
-        Animal animal2 = new Dog("Max");
+        Animal animal2 = new Dog("Puppy");
         animal2.sleep();
         animal2.makeSound();
         animal2.eat();
